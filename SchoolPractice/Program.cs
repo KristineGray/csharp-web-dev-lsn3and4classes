5211c2ba587ff405e5e85640b8e678001269a7fb
KristineGray <krisgraya@gmail.com>
2021-06-28 17:19:47 -0400

    completed exercise

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SchoolPractice
 {
@@ -7,6 +8,38 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            Student studentOne = new Student();
+            studentOne.Name = "Kris";
+            studentOne.StudentId = 1001;
+            studentOne.NumberOfCredits = 1;
+            studentOne.Gpa = 4.0;
+
+            Student studentTwo = new Student();
+            studentTwo.Name = "Bob";
+            studentTwo.StudentId = 1002;
+
+            Student studentThree = new Student();
+            studentThree.Name = "Cody";
+            studentThree.StudentId = 1003;
+
+            Course programming = new Course();
+            programming.Name = "Programming 101";
+            programming.NumStudents = 3;
+
+            List<Student> students = new List<Student> { studentOne, studentTwo, studentThree };
+            foreach (Student student in students)
+            {
+                programming.AddStudent(student.StudentId, student.Name);
+            }
+            programming.PrintRoster();
+
+            Teacher teacher = new Teacher();
+            teacher.FirstName = "Roger";
+            teacher.LastName = "Campbell";
+            teacher.Subject = "Programming";
+            teacher.YearsTeaching = 12; //Don't remember
+            teacher.PrintTeacher();
+            Console.ReadKey();
         }
     }
 }
--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 // Start working here with your Student class.
 // To instantiate the Student class, add your code to the Main method in Program
@@ -7,9 +8,41 @@ namespace SchoolPractice
 {
     public class Student
     {
-        private string Name;
-        private int StudentId;
-        private int NumberOfCredits;
-        private double Gpa;
+        public string Name { get; set; }
+        public int StudentId { get; set; }
+        public int NumberOfCredits { get; set; }
+        public double Gpa { get; set; }
+    }
+
+    public class Course
+    {
+        public string Name { get; set; }
+        public int NumStudents { get; set; }
+        public Dictionary<int, string> Roster = new Dictionary<int, string>();
+
+        public void AddStudent(int studentID, string studentName)
+        {
+            this.Roster.Add(studentID, studentName);
+            //Console.WriteLine($"Student #:{studentID} - {studentName} added to roster.");
+        }
+        public void PrintRoster()
+        {
+            Console.WriteLine($"\nThe student roster for {this.Name}:");
+            foreach (KeyValuePair <int, string> student in Roster)
+                Console.WriteLine($"Student ID #{student.Key} - Name: {student.Value}");
+        }
+    }
+
+    public class Teacher
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Subject { get; set; }
+        public int YearsTeaching { get; set; }
+
+        public void PrintTeacher()
+        {
+            Console.WriteLine($"\n{FirstName} {LastName} teaches {Subject} and has {YearsTeaching} years of experience.");
+        }
     }
 }
